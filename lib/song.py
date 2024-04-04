9338c78b004b76b43b8b10af28c9ddf3c6afbefc
Keenan Weise <kcweise@gmail.com>
2024-04-04 13:57:42 -0500

    Done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,59 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    genre_count = {}
+    artists = []
+    artist_count = {}
+
+    #def song_count (self):
+        
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        self.__class__.add_song_to_count()
+        self.__class__.add_to_genres(genre)
+        self.__class__.add_to_artists(artist)
+        self.__class__.add_to_genre_count(genre)
+        self.__class__.add_to_artist_count(artist)
+
+
+
+    @classmethod  
+    def add_song_to_count(cls):
+        cls.count+=1
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre in cls.genres:
+            print("genre already exists")
+        else:
+            cls.genres.append(genre)
+            print(f"Added {genre} to genres list.")
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist in cls.artists:
+            print ("artist already exists")
+        else:
+            cls.artists.append(artist)
+            print(f"Added {artist} to artists list")
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+
